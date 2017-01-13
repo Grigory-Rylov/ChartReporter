7099cbf1271fb04fd07edddb6858496955618607
Grigory Rylov <grishberg@MBP-Grigory.lan>
2017-01-13 09:04:44 +0700

    fixed end point

--- app/src/main/java/com/grishberg/graphreporter/data/RestConst.java
+++ app/src/main/java/com/grishberg/graphreporter/data/RestConst.java
@@ -5,7 +5,7 @@ package com.grishberg.graphreporter.data;
  */
 public class RestConst {
     private static final String TAG = RestConst.class.getSimpleName();
-    public final static String END_POINT = "http://127.0.0.1:5000";
+    public final static String END_POINT = "https://Grishberg.pythonanywhere.com";
     public static final String API = END_POINT + "/analytics/api/v1.0/";
 
     // Ошибки
