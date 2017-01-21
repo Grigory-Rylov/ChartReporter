2d8f4279d42f3e0ae3858fa753f017389dad53ca
Grigory Rylov <grishberg@MBP-Grigory.lan>
2017-01-22 00:34:38 +0700

    fixed storing for different products

--- app/src/main/java/com/grishberg/graphreporter/data/repository/values/InMemoryDailyDataStorage.java
+++ app/src/main/java/com/grishberg/graphreporter/data/repository/values/InMemoryDailyDataStorage.java
@@ -1,9 +1,13 @@
 package com.grishberg.graphreporter.data.repository.values;
 
+import android.util.SparseArray;
+
 import com.grishberg.graphreporter.data.model.DailyValue;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import rx.Observable;
 
@@ -12,26 +16,33 @@ import rx.Observable;
  * Реализация хранилища данных в памяти
  */
 public class InMemoryDailyDataStorage implements DailyDataStorage {
-    private final ArrayList<DailyValue> cache;
+    private final Map<Long, List<DailyValue>> cache;
     private final CacheActualityChecker cacheChecker;
 
     public InMemoryDailyDataStorage(final CacheActualityChecker cacheChecker) {
-        cache = new ArrayList<>();
+        cache = new ConcurrentHashMap<>();
         this.cacheChecker = cacheChecker;
     }
 
     @Override
     public void setDailyData(final long productId, final List<DailyValue> values) {
-        cache.clear();
-        cache.addAll(values);
+        List<DailyValue> list = cache.get(productId);
+        if (list != null) {
+            cache.clear();
+        } else {
+            list = new ArrayList<>(300);
+            cache.put(productId, list);
+        }
+        list.addAll(values);
         cacheChecker.updateNewData(productId);
     }
 
     @Override
     public Observable<List<DailyValue>> getDailyValues(final long productId) {
         if (cacheChecker.isCacheDataValid(productId)) {
-            return Observable.just(cache);
+            return Observable.just(cache.get(productId));
         }
+        cache.remove(productId);
         return Observable.just(null);
     }
 }
