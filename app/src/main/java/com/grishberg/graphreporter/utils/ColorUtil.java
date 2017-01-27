84ba0380191b22c360dd01cf00d46637699cdf3b
grishberg <grishberg@MBP-Grigory.lan>
2017-01-27 13:52:19 +0700

    fixed color issues

--- app/src/main/java/com/grishberg/graphreporter/utils/ColorUtil.java
+++ app/src/main/java/com/grishberg/graphreporter/utils/ColorUtil.java
@@ -12,7 +12,7 @@ import static android.os.Build.VERSION_CODES.LOLLIPOP;
 public class ColorUtil {
     public static int getColor(final Context context, final int colorResId) {
         if (SDK_INT >= LOLLIPOP) {
-            return ContextCompat.getColor(context, colorResId)
+            return ContextCompat.getColor(context, colorResId);
         } else {
             return context.getResources().getColor(colorResId);
         }
