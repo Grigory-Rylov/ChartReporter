ec3941d22a234e7a25e5452855d85b1d949eb23e
grishberg <grishberg@MBP-Grigory.lan>
2017-01-29 12:14:58 +0700

    добавил ButterKnife, реализовал диалог добавление формулы

---
+++ app/src/main/java/com/grishberg/graphreporter/data/model/FormulaContainer.java
@@ -0,0 +1,54 @@
+package com.grishberg.graphreporter.data.model;
+
+import java.io.Serializable;
+
+/**
+ * Created by grishberg on 29.01.17.
+ * Контейнер для хранения формулы
+ */
+public class FormulaContainer implements Serializable {
+    public enum VertexType {
+        OPEN,
+        CLOSED,
+        MIN,
+        MAX
+    }
+
+    private final String name;
+    private final VertexType vertexType;
+    private final double formulaValue;
+    private final boolean isPercent;
+    private final boolean isGreater;
+
+    public FormulaContainer(final String name,
+                            final VertexType vertexType,
+                            final double formulaValue,
+                            final boolean isPercent,
+                            final boolean isGreater) {
+        this.name = name;
+        this.vertexType = vertexType;
+        this.formulaValue = formulaValue;
+        this.isPercent = isPercent;
+        this.isGreater = isGreater;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public VertexType getVertexType() {
+        return vertexType;
+    }
+
+    public double getFormulaValue() {
+        return formulaValue;
+    }
+
+    public boolean isPercent() {
+        return isPercent;
+    }
+
+    public boolean isGreater() {
+        return isGreater;
+    }
+}
--- app/src/main/java/com/grishberg/graphreporter/ui/dialogs/NewPointDialog.java
+++ app/src/main/java/com/grishberg/graphreporter/ui/dialogs/NewPointDialog.java
@@ -1,26 +1,45 @@
 package com.grishberg.graphreporter.ui.dialogs;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 
+import com.grishberg.graphreporter.R;
+import com.grishberg.graphreporter.data.model.FormulaContainer;
 import com.grishberg.graphreporter.mvp.common.BaseMvpDialogFragment;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by grishberg on 28.01.17.
+ * Диалог добавления формулы
  */
-public class NewPointDialog extends BaseMvpDialogFragment {
-    private static final String TAG = NewPointDialog.class.getSimpleName();
+public class NewPointDialog extends BaseMvpDialogFragment implements View.OnClickListener {
     public static final int NEW_POINT_RESULT_CODE = 1001;
+    private static final String NEW_POINT_RESULT_EXTRA = NewPointDialog.class.getSimpleName();
+    @BindView(R.id.dialog_new_point_name)
+    EditText pointName;
+    @BindView(R.id.dialog_new_point_percent)
+    EditText pointPercent;
 
-    private Spinner spinner;
+    private Spinner vertexSpinner;
+    private Spinner conditionSpinner;
 
     public static void showDialog(final FragmentManager fm,
-                                  final Fragment targetFragment,
-                                  final int lastPosition) {
+                                  final Fragment targetFragment) {
         final FragmentTransaction ft = fm.beginTransaction();
         final Fragment prev = fm
                 .findFragmentByTag(NewPointDialog.class.getSimpleName());
@@ -36,5 +55,66 @@ public class NewPointDialog extends BaseMvpDialogFragment {
         newFragment.show(ft, PeriodSelectDialog.class.getSimpleName());
     }
 
+    @Override
+    public View onCreateView(final LayoutInflater inflater,
+                             final ViewGroup container,
+                             final Bundle savedInstanceState) {
+        final View view = inflater.inflate(R.layout.dialog_new_point, container, false);
+        initVertexSpinner(view);
+        initConditionSpinner(view);
+        initButton(view);
+        ButterKnife.bind(this, view);
+        return view;
+    }
 
+    private void initVertexSpinner(final View view) {
+        vertexSpinner = (Spinner) view.findViewById(R.id.dialog_new_point_items_spinner);
+        vertexSpinner.setAdapter(createVertexSpinnerAdapter());
+    }
+
+    protected BaseAdapter createVertexSpinnerAdapter() {
+        final ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(),
+                R.array.vertex_name_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        return adapter;
+    }
+
+    private void initConditionSpinner(final View view) {
+        conditionSpinner = (Spinner) view.findViewById(R.id.dialog_new_point_condition_spinner);
+        conditionSpinner.setAdapter(createConditionSpinnerAdapter());
+    }
+
+    protected BaseAdapter createConditionSpinnerAdapter() {
+        final ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(),
+                R.array.condition_name_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        return adapter;
+    }
+
+    private void initButton(final View view) {
+        final Button okButton = (Button) view.findViewById(R.id.dialog_new_point_ok_button);
+        okButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(final View view) {
+        final Intent data = new Intent();
+        data.putExtra(NEW_POINT_RESULT_EXTRA,
+                new FormulaContainer(
+                        pointName.getText().toString(),
+                        FormulaContainer.VertexType.values()[vertexSpinner.getSelectedItemPosition()],
+                        Double.valueOf(pointPercent.getText().toString()),
+                        true,
+                        conditionSpinner.getSelectedItemPosition() == 0));
+        getTargetFragment().onActivityResult(getTargetRequestCode(), 0, data);
+        dismiss();
+    }
+
+    @Nullable
+    public static FormulaContainer getResult(final Intent intent) {
+        if (intent != null) {
+            return (FormulaContainer) intent.getSerializableExtra(NEW_POINT_RESULT_EXTRA);
+        }
+        return null;
+    }
 }
